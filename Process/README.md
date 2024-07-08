b153f8439daf1ec09f28c10a634b168c139f199f
star-cs <2572523998@qq.com>
2024-07-08 23:15:50 +0800

    CreateProcess

---
+++ Process/README.md
@@ -0,0 +1,45 @@
+# 线程Process
+
+## CreateProcess
+
+类似于 fork() + exec()
+
+```c++
+WINBASEAPI
+BOOL
+WINAPI
+CreateProcessW(
+    _In_opt_ LPCSTR lpApplicationName,
+    _Inout_opt_ LPSTR lpCommandLine,
+    _In_opt_ LPSECURITY_ATTRIBUTES lpProcessAttributes,     
+    _In_opt_ LPSECURITY_ATTRIBUTES lpThreadAttributes,
+    _In_ BOOL bInheritHandles,
+    _In_ DWORD dwCreationFlags,
+    _In_opt_ LPVOID lpEnvironment,
+    _In_opt_ LPCSTR lpCurrentDirectory,
+    _In_ LPSTARTUPINFOA lpStartupInfo,
+    _Out_ LPPROCESS_INFORMATION lpProcessInformation
+    );
+```
+
+    lpApplicationName：要执行的模块（可执行文件）的名称。
+
+    lpCommandLine：传递给新创建进程的命令行。这可以包括任何命令行参数。
+
+    lpProcessAttributes：指向SECURITY_ATTRIBUTES结构的指针，该结构确定返回的新进程对象句柄是否可以被子进程继承。（说明是内核对象，NULL）
+
+    lpThreadAttributes：与lpProcessAttributes类似，但针对线程对象。
+
+    bInheritHandles：如果此值为TRUE，则新进程将继承调用进程的所有打开的句柄。
+
+    dwCreationFlags：指定新进程的创建标志。常见的值包括CREATE_NEW_CONSOLE（创建新的控制台窗口）、CREATE_NO_WINDOW（防止创建窗口）和DETACHED_PROCESS（从当前作业对象分离进程）。
+
+    lpEnvironment：指向新进程环境块的指针。如果为NULL，则新进程使用父进程的环境。
+
+    lpCurrentDirectory：新进程的当前目录的完整路径。如果为NULL，则新进程使用父进程的当前目录。
+
+    lpStartupInfo：指向STARTUPINFO结构的指针，用于设置新进程的启动信息，如控制台窗口的大小、位置等。
+
+    lpProcessInformation：指向PROCESS_INFORMATION结构的指针，用于接收新进程的信息，如进程ID和主线程ID。
+
+     
---
+++ Process/main.cpp
@@ -0,0 +1,66 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main()
+{
+    WINBASEAPI
+        BOOL
+        WINAPI
+        CreateProcessW(
+            _In_opt_ LPCWSTR lpApplicationName, //NULL
+            _Inout_opt_ LPWSTR lpCommandLine,   
+            _In_opt_ LPSECURITY_ATTRIBUTES lpProcessAttributes,
+            _In_opt_ LPSECURITY_ATTRIBUTES lpThreadAttributes,
+            _In_ BOOL bInheritHandles,
+            _In_ DWORD dwCreationFlags,
+            _In_opt_ LPVOID lpEnvironment,
+            _In_opt_ LPCWSTR lpCurrentDirectory,
+            _In_ LPSTARTUPINFOW lpStartupInfo,
+            _Out_ LPPROCESS_INFORMATION lpProcessInformation
+        );
+
+    // 命令行 拉起网站。L"\"...\...\xxx.exe"..."
+    TCHAR szCommandLine[] = L"\"C:\\Program Files\\Google\\Chrome\\Application\\Chrome.exe\"http://www.github.com/star-cs";
+
+    STARTUPINFOW strStartupInfo;
+    memset(&strStartupInfo, 0, sizeof(strStartupInfo));
+    strStartupInfo.cb = sizeof(strStartupInfo);
+
+    PROCESS_INFORMATION szProcessInformation;
+    memset(&szProcessInformation, 0, sizeof(szProcessInformation));
+
+    bool bRet = CreateProcess(NULL,
+        szCommandLine,
+        NULL,
+        NULL,
+        FALSE,                         //是否继承父进程的句柄
+        CREATE_NEW_CONSOLE,
+        NULL,
+        NULL,
+        &strStartupInfo,
+        &szProcessInformation
+    );
+
+    if (bRet)
+    {
+        printf("Create Sucess bRet = %d\n", bRet);
+        WaitForSingleObject(szProcessInformation.hThread, 3000);
+        printf("Create Sucess szProcessInformation.hProcess = %d\n", szProcessInformation.hProcess);
+        printf("Create Sucess szProcessInformation.hThread = %d\n", szProcessInformation.hThread);
+        printf("Create Sucess szProcessInformation.dwProcessId = %d\n", szProcessInformation.dwProcessId);
+        printf("Create Sucess szProcessInformation.dwThreadId = %d\n", szProcessInformation.dwThreadId);
+
+        CloseHandle(szProcessInformation.hProcess);
+        CloseHandle(szProcessInformation.hThread);
+        szProcessInformation.hProcess = nullptr;
+        szProcessInformation.hThread = nullptr;
+        szProcessInformation.dwProcessId = 0;
+        szProcessInformation.dwThreadId = 0;
+    }
+    else
+    {
+        printf("Create Failed bRet = %d，errorno = %d\n", bRet, GetLastError());
+    }
+
+    return 0;
+}
